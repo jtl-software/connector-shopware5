0db49f9018cf6cad5bb5784dd5f00e02a1d37bc8
Daniel Böhmer <daniel.boehmer@jtl-software.com>
2015-10-15 12:15:24 +0200

    push to version 1.2.2

--- Bootstrap.php
+++ Bootstrap.php
@@ -27,7 +27,7 @@ class Shopware_Plugins_Frontend_jtlconnector_Bootstrap extends Shopware_Componen
 
     public function getVersion()
     {
-        return '1.2.1';
+        return '1.2.2';
     }
 
     public function getInfo()
