ced2dbc85e743a73904a1fb2751cb445320e591e
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2019-04-26 11:37:57 +0200

    Security check added.

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-ulimit -n 100000;
-composer update --no-dev;
-php ./vendor/bin/phing release;
-composer update;
+clear;
+printf "Hast du daran gedacht die Update Methode in der Bootstrap.php zu ergänzen? [j/N] : ";
+read -r cmd;
+
+if [[ $cmd = 'j' || $cmd = 'J' ]]; then
+    ulimit -n 100000;
+    composer update --no-dev;
+    php ./vendor/bin/phing release;
+    composer update;
+fi
