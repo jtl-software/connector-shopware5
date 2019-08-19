b34fff17b3ee34b11add5b7cb7c81604457b9395
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2019-08-19 11:17:44 +0200

    Changelog and version updated.

--- Bootstrap.php
+++ Bootstrap.php
@@ -299,6 +299,7 @@ class Shopware_Plugins_Frontend_jtlconnector_Bootstrap extends Shopware_Componen
             case '2.2.1.3':
             case '2.2.1.4':
             case '2.2.2':
+            case '2.2.3':
                 break;
             default:
                 return false;
