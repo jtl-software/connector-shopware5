d20f59a7f8dc51f1d5761f82689235f4a1798e9c
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2020-08-05 09:57:01 +0200

    Removed delivery status parsing

--- src/Mapper/Product.php
+++ src/Mapper/Product.php
@@ -723,21 +723,7 @@ class Product extends DataMapper
         $detailSW->setHeight($product->getHeight());
 
         // Delivery time
-        $exists = false;
-        foreach ($product->getI18ns() as $i18n) {
-            if ($i18n->getLanguageISO() === LanguageUtil::map(Shopware()->Shop()->getLocale()->getLocale())) {
-                $days = trim(str_replace(['Tage', 'Days', 'Tag', 'Day'], '', $i18n->getDeliveryStatus()));
-                if (strlen($days) > 0 && $days !== '0') {
-                    $detailSW->setShippingTime($days);
-                    $exists = true;
-                    break;
-                }
-            }
-        }
-
-        if (!$exists) {
-            $detailSW->setShippingTime($product->calculateHandlingTime());
-        }
+        $detailSW->setShippingTime($product->calculateHandlingTime());
 
         // Last stock
         $inStock = 0;
