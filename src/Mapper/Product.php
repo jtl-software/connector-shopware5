4f6311b786ffa7b03d9ead126e2e2b2cc691c538
Patryk Gorol <patryk.gorol@jtl-software.com>
2022-02-01 11:13:25 +0100

    Moved max purchase attribute

--- src/Mapper/Product.php
+++ src/Mapper/Product.php
@@ -1030,6 +1030,11 @@ class Product extends DataMapper
                 continue;
             }
 
+            if ($lcAttributeName === ProductAttr::MAX_PURCHASE) {
+                $detailSW->setMaxPurchase($attributeValue);
+                continue;
+            }
+
             if (!$isChild && $lcAttributeName === ProductAttr::CUSTOM_PRODUCTS_TEMPLATE) {
                 $pluginName = "SwagCustomProducts";
                 /** @var Plugin $plugin */
@@ -1048,10 +1053,6 @@ class Product extends DataMapper
                         ShopUtil::entityManager()->persist($template);
                     }
                 }
-            }
-
-            if ($lcAttributeName === ProductAttr::MAX_PURCHASE) {
-                $detailSW->setMaxPurchase($attributeValue);
             }
 
             $mappings[$attributeI18n->getName()] = $attribute->getId()->getHost();
