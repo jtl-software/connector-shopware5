b429502aa06df8a8ff98fabe443466b9dd438259
Cem Aktas <cem.aktas@jtl-software.com>
2023-04-17 14:35:58 +0200

    update code style in authenticate, checksum and service
    (cherry picked from commit 10215785f7b5cd3473e21ab25665fb7fe3316e7d)

--- src/Checksum/ChecksumLoader.php
+++ src/Checksum/ChecksumLoader.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright 2010-2013 JTL-Software GmbH
  * @package jtl\Connector\Shopware\Controller
@@ -6,9 +7,9 @@
 
 namespace jtl\Connector\Shopware\Checksum;
 
-use \jtl\Connector\Checksum\IChecksumLoader;
-use \jtl\Connector\Shopware\Utilities\IdConcatenator;
-use \jtl\Connector\Core\Logger\Logger;
+use jtl\Connector\Checksum\IChecksumLoader;
+use jtl\Connector\Shopware\Utilities\IdConcatenator;
+use jtl\Connector\Core\Logger\Logger;
 
 final class ChecksumLoader implements IChecksumLoader
 {
@@ -19,14 +20,17 @@ final class ChecksumLoader implements IChecksumLoader
         }
 
         list ($detailId, $productId) = IdConcatenator::unlink($endpointId);
-
-        $result = Shopware()->Db()->fetchOne(
-            'SELECT checksum FROM jtl_connector_product_checksum WHERE product_id = ? AND detail_id = ? AND type = ?',
-            array($productId, $detailId, $type)
+        $result                      = \Shopware()->Db()
+            ->fetchOne(
+                'SELECT checksum FROM jtl_connector_product_checksum
+                WHERE product_id = ? AND detail_id = ? AND type = ?',
+                array($productId, $detailId, $type)
+            );
+        Logger::write(
+            \sprintf('Read with endpointId (%s), type (%s) - result (%s)', $endpointId, $type, $result),
+            Logger::DEBUG,
+            'checksum'
         );
-
-        Logger::write(sprintf('Read with endpointId (%s), type (%s) - result (%s)', $endpointId, $type, $result), Logger::DEBUG, 'checksum');
-
         return ($result !== false) ? $result : '';
     }
 
@@ -37,19 +41,19 @@ final class ChecksumLoader implements IChecksumLoader
         }
 
         list ($detailId, $productId) = IdConcatenator::unlink($endpointId);
-
-        $sql = '
+        $sql                         = '
             INSERT IGNORE INTO jtl_connector_product_checksum
             (
                 product_id, detail_id, type, checksum
             )
             VALUES (?,?,?,?)
         ';
-
-        Logger::write(sprintf('Write with endpointId (%s), type (%s) - checksum (%s)', $endpointId, $type, $checksum), Logger::DEBUG, 'checksum');
-
-        $statement = Shopware()->Db()->query($sql, array($productId, $detailId, $type, $checksum));
-
+        Logger::write(
+            \sprintf('Write with endpointId (%s), type (%s) - checksum (%s)', $endpointId, $type, $checksum),
+            Logger::DEBUG,
+            'checksum'
+        );
+        $statement = \Shopware()->Db()->query($sql, array($productId, $detailId, $type, $checksum));
         return $statement ? true : false;
     }
 
@@ -60,11 +64,16 @@ final class ChecksumLoader implements IChecksumLoader
         }
 
         list ($detailId, $productId) = IdConcatenator::unlink($endpointId);
-
-        $rows = Shopware()->Db()->delete('jtl_connector_product_checksum', array('product_id = ?' => $productId, 'detail_id = ?' => $detailId));
-
-        Logger::write(sprintf('Delete with endpointId (%s), type (%s)', $endpointId, $type), Logger::DEBUG, 'checksum');
-
+        $rows                        = \Shopware()->Db()
+            ->delete(
+                'jtl_connector_product_checksum',
+                array('product_id = ?' => $productId, 'detail_id = ?' => $detailId)
+            );
+        Logger::write(
+            \sprintf('Delete with endpointId (%s), type (%s)', $endpointId, $type),
+            Logger::DEBUG,
+            'checksum'
+        );
         return $rows ? true : false;
     }
 }
